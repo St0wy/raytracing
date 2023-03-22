c5ab46148a8af3555596c20b1e8c76603a7bc7d8
Stowy <stowy@protonmail.ch>
2023-03-22 16:58:35 +0100

    fixed last benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -34,10 +34,10 @@ fn bench_cornell_box(c: &mut Criterion) {
     });
 }
 
-fn bench_(c: &mut Criterion) {
-    let scene = Scene::cornell_box();
+fn bench_perlin_and_earth(c: &mut Criterion) {
+    let scene = Scene::perlin_and_earth();
 
-    c.bench_function("render cornell_box", |b| {
+    c.bench_function("render perlin_and_earth", |b| {
         b.iter(|| {
             render_no_bar_multithreaded(black_box(&scene), IMAGE_WIDTH, IMAGE_HEIGHT);
         })
@@ -47,6 +47,6 @@ fn bench_(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().measurement_time(Duration::from_secs_f64(170.0));
-    targets = bench_three_spheres, bench_big_scene, bench_cornell_box
+    targets = bench_three_spheres, bench_big_scene, bench_cornell_box, bench_perlin_and_earth
 }
 criterion_main!(benches);
