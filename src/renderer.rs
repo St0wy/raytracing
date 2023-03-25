d0a119068334e7182c1319fe026dd8172018d92f
Stowy <stowy@protonmail.ch>
2023-03-25 17:03:55 +0100

    added multithreading

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use rayon::prelude::*;
 
 use crate::consts::{MAX_DEPTH, SAMPLES_PER_PIXEL};
 use crate::geometry::hittable_world::HittableWorld;
@@ -51,46 +52,40 @@ fn ray_color(
     emitted + scatter_result.attenuation * ray_color
 }
 
-/// Writes the color to the pixels vector.
-///
-/// # Arguments
-///
-/// * `pixels`: Pixels vector to write the color into.
-/// * `color`: Color to write in the pixels vector.
-///
-/// returns: ()
-fn write_color(pixels: &mut Vec<u8>, color: Color) {
-    let scale = 1.0 / SAMPLES_PER_PIXEL as f32;
-    for k in 0..3 {
-        let scaled_color = (color[k as usize] * scale).sqrt();
-        pixels.push((256.0 * scaled_color.clamp(0.0, 0.999)) as u8);
-    }
-}
-
 pub fn render(scene: &Scene, image_width: usize, image_height: usize) -> Vec<u8> {
-    let mut rng = rand::thread_rng();
+    let pixels: Vec<u8> = (0..image_height)
+        .into_par_iter()
+        .rev()
+        .flat_map(|j| {
+            (0..image_width)
+                .into_par_iter()
+                .flat_map(|i| {
+                    let mut pixel_color = Color::black();
+                    for _ in 0..SAMPLES_PER_PIXEL {
+                        let mut rng = rand::thread_rng();
+                        let u = (i as f32 + rng.gen::<f32>()) / (image_width as f32 - 1.0);
+                        let v = (j as f32 + rng.gen::<f32>()) / (image_height as f32 - 1.0);
+                        let ray = scene.camera().get_ray(u, v);
 
-    let mut pixels = Vec::new();
-    pixels.reserve(image_width * image_height);
-    for j in (0..image_height).rev() {
-        for i in 0..image_width {
-            let mut pixel_color = Color::black();
-            for _ in 0..SAMPLES_PER_PIXEL {
-                let u = (i as f32 + rng.gen::<f32>()) / (image_width as f32 - 1.0);
-                let v = (j as f32 + rng.gen::<f32>()) / (image_height as f32 - 1.0);
-                let ray = scene.camera().get_ray(u, v);
+                        pixel_color += ray_color(
+                            &ray,
+                            scene.background_color(),
+                            scene.hittable_list(),
+                            MAX_DEPTH,
+                        );
+                    }
 
-                pixel_color += ray_color(
-                    &ray,
-                    scene.background_color(),
-                    scene.hittable_list(),
-                    MAX_DEPTH,
-                );
-            }
-
-            write_color(&mut pixels, pixel_color);
-        }
-    }
+                    const SCALE: f32 = 1.0 / SAMPLES_PER_PIXEL as f32;
+                    (0..3)
+                        .map(|k| {
+                            (256.0 * (pixel_color[k as usize] * SCALE).sqrt().clamp(0.0, 0.999))
+                                as u8
+                        })
+                        .collect::<Vec<u8>>()
+                })
+                .collect::<Vec<u8>>()
+        })
+        .collect::<Vec<u8>>();
 
     pixels
 }
