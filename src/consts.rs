90d24a96b97aaf22b7803341d7b0dfc9db5e7fc5
Stowy <stowy@protonmail.ch>
2023-04-02 18:38:36 +0200

    updated consts

--- src/consts.rs
+++ src/consts.rs
@@ -6,5 +6,5 @@ pub const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as usize;
 pub const VIEWPORT_HEIGHT: f32 = 2.0;
 pub const VIEWPORT_WIDTH: f32 = ASPECT_RATIO * VIEWPORT_HEIGHT;
 pub const FOCAL_LENGTH: f32 = 1.0;
-pub const SAMPLES_PER_PIXEL: i32 = 500;
-pub const MAX_DEPTH: u32 = 50;
+pub const SAMPLES_PER_PIXEL: i32 = 300;
+pub const MAX_DEPTH: u32 = 30;
